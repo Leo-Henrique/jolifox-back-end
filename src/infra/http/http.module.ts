c6ad82a0e0d9e4b9ba606ecf24590ce44d68e17e
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-11-07 21:09:42 -0300

    refactor: clean up unused files from @leo-h/create-nodejs-app

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,16 +2,13 @@ import { Module } from "@nestjs/common";
 import { APP_FILTER } from "@nestjs/core";
 import { DatabaseModule } from "../database/database.module";
 import { CreateCampaignController } from "./controllers/campaign/create-campaign.controller";
-import { UpdateCampaignController } from "./controllers/campaign/update-campaign.controller";
-import { HelloMultipartController } from "./controllers/hello/hello-multipart.controller";
-import { HelloController } from "./controllers/hello/hello.controller";
-import { AllExceptionFilter } from "./errors/filters/all-exception.filter";
-import { FastifyMulterEventModule } from "./events/fastify-multer.event.module";
 import { DeleteCampaignController } from "./controllers/campaign/delete-campaign.controller";
 import { GetCampaignController } from "./controllers/campaign/get-campaign.controller";
+import { UpdateCampaignController } from "./controllers/campaign/update-campaign.controller";
+import { AllExceptionFilter } from "./errors/filters/all-exception.filter";
 
 @Module({
-  imports: [FastifyMulterEventModule, DatabaseModule],
+  imports: [DatabaseModule],
   providers: [
     {
       provide: APP_FILTER,
@@ -19,8 +16,6 @@ import { GetCampaignController } from "./controllers/campaign/get-campaign.contr
     },
   ],
   controllers: [
-    HelloController,
-    HelloMultipartController,
     CreateCampaignController,
     UpdateCampaignController,
     DeleteCampaignController,
